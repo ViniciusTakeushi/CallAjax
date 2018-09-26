3b9512dd8b8acef69950f22680cd96b7c3ca8eb3
Vinicius Kendi Takeushi <vinicius.takeushi@outlook.com>
2018-09-26 00:21:57 -0300

    Add project files.

---
+++ CallAjax/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CallAjax.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult JsonTeste(Teste teste)
+        {
+            var result = new ResultCall();
+            result.Message = "Ok";
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult JsonTesteGetData()
+        {
+            var result = new Teste();
+            result.Nome = "AAAA";
+            result.Sobrenome = "BBBB";
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class Teste
+    {
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+    }
+
+    public class ResultCall
+    {
+        public string Message { get; set; }
+    }
+}
